1025da1e3297def626bbae5a5af7635a1f701dfd
fjthieleng <franthielengaravito@gmail.com>
2022-11-17 01:25:44 -0400

    Cambios correspondientes al ejercicio 8

--- ejercicio8.py
+++ ejercicio8.py
@@ -1,4 +1,7 @@
 
+'''
+En este ejercicio, tendréis que crear un archivo py donde creéis un archivo txt, lo abráis y escribáis dentro del archivo. Para ello, tendréis que acceder dos veces al archivo creado.
+'''
 # Creo el archivo
 f = open('myFile.txt', 'x')
 f.close()
